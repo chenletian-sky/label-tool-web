040ee7f453caea1505a94fb8db04a6e6a36c50d3
chenletian-sky <446075873@qq.com>
2021-12-21 19:01:15 +0800

    wordRecognition 识别 修改并调试成功

--- src/components/toolPages/WordRecognition.tsx
+++ src/components/toolPages/WordRecognition.tsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 //antd组件库
-import {Button, Table, Form, Input, Slider, Dropdown, Menu} from 'antd';
+import {Button, Table, Form, Input, Slider, Dropdown, Menu, message} from 'antd';
 import { DownOutlined, UserOutlined } from '@ant-design/icons';
 import '../current/current'
 import TrainMarkView from './TrainMarkView'
@@ -91,7 +91,7 @@ class WordRecognition extends Component<WordRecognitionProps, WordRecognitionSta
               {
                 (value:any) => {
                   this.setState({
-                    SliderVal:value.target.value
+                    SliderVal:value
                   })
                 }
               }/>
@@ -138,57 +138,67 @@ class WordRecognition extends Component<WordRecognitionProps, WordRecognitionSta
                   // axios.post(`http://101.35.15.228:8080/mongo/texts/upload`, upLoadData)
                   //   .then((res:AxiosResponse<any, any>) => {
                   // })
-                  console.log(`${PATH}/mongo/utils/dbDictSplit`);
+                  // console.log(`${PATH}/mongo/utils/dbDictSplit`);
                   
                   
-                  
-                  axios.post(`${PATH}/mongo/utils/dbDictSplit`, {dictKey:this.props.LoadingDataCom.dictionarySelectObject?.selectedRowKeys[0].key,textsKey:"-200"})
-                    .then((res:AxiosResponse<any, any>) => {
-                      if(res.data.status === 200)
-                      console.log("2")
-                      console.log("2")
+                  console.log("dbDictSplit",this.props.LoadingDataCom.dictionarySelectObject?.selectedRows[0].key)
+                  axios.post(`${PATH}/mongo/utils/dbDictSplit`, {dictKey:this.props.LoadingDataCom.dictionarySelectObject?.selectedRows[0].key,textsKey:"-200"})
+                    .then((firstRes:AxiosResponse<any, any>) => {
+                      if(firstRes.data.status === 200){
+                        message.success("字典分词调用成功")
+
+                        axios.post(`${PATH}/mongo/utils/jiaguTrainModel`, {textsKey:"-200",numberOfTrainingIterations:-1})
+                          .then((secondRes:AxiosResponse<any, any>) => {
+                            if(secondRes.data.status === 200 ){
+                              message.success("jiagu 调用成功")
+                              axios.get(`${PATH}/mongo/xferStations/all`)
+                                .then((res:AxiosResponse<any, any>) => {
+                                  if(res.data.status === 200 ){
+                                    console.log(res.data.data.data.now)
+                                    const fileData = res.data.data.data.now
+                                    const after =  fileData.map((value:any, i: string)=>{
+                                      let returnValue = {
+                                          text: value['text'],
+                                          key: Number(Math.random().toString().substr(3, 10) + Date.now()).toString(36),
+                                          textArr: value['text'].split('').map((v: any, index: any) => ({
+                                              text: v,
+                                              start: index,
+                                              end: index,
+                                              label: 'none',
+                                              color: '',
+                                          }))
+                                      }
+                                      for(let i = value['labels'].length - 1; i >= 0; i--) {
+                                          const { start, end, label } = value['labels'][i]
+                                          // console.log("each",start,end,label)
+                                          returnValue['textArr'].splice(start, end - start)
+                                          returnValue['textArr'].splice(start, 0, {
+                                              text: value['text'].slice(start, end),
+                                              start,
+                                              end: end - 1,
+                                              label,
+                                              color:"red"
+                                          })
+                                      }
+
+                                      return returnValue
+                                    })
+                                    this.setState({MarkTexts: after})
+                                  }
+                                  
+                              })
+                            }
+                            
+                        })
+                      }
+
                   })
                   .catch((error)=>{
                     console.log(error);
                   })
-                  axios.post(`${PATH}/mongo/utils/jiaguTrainModel`, {textsKey:"-200",numberOfTrainingIterations:-1})
-                    .then((res:AxiosResponse<any, any>) => {
-                      console.log("3")
-                  })
-                  axios.get(`${PATH}/mongo/xferStations/all`)
-                    .then((res:AxiosResponse<any, any>) => {
-                      console.log(res.data.data.data.now)
-                      const fileData = res.data.data.data.now
-                      const after =  fileData.map((value:any, i: string)=>{
-                        let returnValue = {
-                            text: value['text'],
-                            key: Number(Math.random().toString().substr(3, 10) + Date.now()).toString(36),
-                            textArr: value['text'].split('').map((v: any, index: any) => ({
-                                text: v,
-                                start: index,
-                                end: index,
-                                label: 'none',
-                                color: '',
-                            }))
-                        }
-                        for(let i = value['labels'].length - 1; i >= 0; i--) {
-                            const { start, end, label } = value['labels'][i]
-                            // console.log("each",start,end,label)
-                            returnValue['textArr'].splice(start, end - start)
-                            returnValue['textArr'].splice(start, 0, {
-                                text: value['text'].slice(start, end),
-                                start,
-                                end: end - 1,
-                                label,
-                                // color:'#d1c7b7'
-                            })
-                        }
-
-                        return returnValue
-                      })
-                      this.setState({MarkTexts: after})
-                  })
-                  console.log(this.state.MarkTexts,"8888")
+                  
+                  
+                  // console.log(this.state.MarkTexts,"8888")
                   // this.setState({MarkTexts:})
                 }
               }
